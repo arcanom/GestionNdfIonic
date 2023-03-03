052b8a50fc3575ca3405e9d5ce577d283ce466c3
Corentin Duriez <corentinduriez22@gmail.com>
2023-03-03 10:58:46 +0100

    Expanse-report from visual ended

--- src/app/formulaire-ndf/formulaire-ndf.page.ts
+++ src/app/formulaire-ndf/formulaire-ndf.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-formulaire-ndf',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormulaireNdfPage implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit() {
   }
 
+  onSubmit(form : NgForm){
+
+  }
+
+  onClick(){
+
+      this.route.navigate(['/connexion'])
+
+  }
 }
