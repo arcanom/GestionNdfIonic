c1715682de5367085f3620f540aeaba1b0fc7711
Corentin Duriez <corentinduriez22@gmail.com>
2023-03-03 11:17:46 +0100

    Treatement and History  Expanse Report visuals ended

--- src/app/historique-ndf/historique-ndf.page.ts
+++ src/app/historique-ndf/historique-ndf.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-historique-ndf',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoriqueNdfPage implements OnInit {
 
-  constructor() { }
+  constructor(private route : Router) { }
 
   ngOnInit() {
+  }
+
+  onClick(){
+    this.route.navigate(['/connexion'])
+
   }
 
 }
--- src/app/traitement-ndf/traitement-ndf.page.ts
+++ src/app/traitement-ndf/traitement-ndf.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-traitement-ndf',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TraitementNdfPage implements OnInit {
 
-  constructor() { }
+  constructor(private route :  Router) { }
 
   ngOnInit() {
   }
+
+  onClick(){
+    this.route.navigate(['/connexion'])
+  }
+
+
 
 }
