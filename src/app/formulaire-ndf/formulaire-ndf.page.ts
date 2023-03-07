1fae94ac6460e1388c2e68b60043abb78adbfebe
Corentin Duriez <corentinduriez22@gmail.com>
2023-03-07 10:12:52 +0100

    Expanse report form with upload file ended

--- src/app/formulaire-ndf/formulaire-ndf.page.ts
+++ src/app/formulaire-ndf/formulaire-ndf.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Ndfrequest } from '../model/ndfrequest';
+import { HttpserviceService } from '../service/httpservice.service';
 
 @Component({
   selector: 'app-formulaire-ndf',
@@ -9,18 +11,80 @@ import { Router } from '@angular/router';
 })
 export class FormulaireNdfPage implements OnInit {
 
-  constructor(private route: Router) { }
+  constructor(private route: Router, private httpService : HttpserviceService ) { }
 
+  categories :  any
+  myFiles : any[] =[]
+  ndf :  Ndfrequest
+  id: any
+  idNdf : number
+  data : any
+  formData : any
+  role: any = "admin"
   ngOnInit() {
+    this.getCategories()
   }
 
-  onSubmit(form : NgForm){
-
+  getCategories(){
+    this.httpService.getAllCategories().subscribe(x=>{
+      this.categories =  x;
+      console.log(this.categories)
+    })
   }
 
   onClick(){
 
-      this.route.navigate(['/connexion'])
+    this.route.navigate(['/connexion'])
+
+}
+
+onFileChange(event: any){
+  for( let i=0; i < event.target.files.length;i++){
+    this.myFiles.push(event.target.files[i])
+  }
+}
+
+  onSubmit(form : NgForm){
+      if(form.valid){
+          this.ndf = {
+            title : form.value.titre,
+            description: form.value.description,
+            amount: form.value.montant,
+            date : form.value.date,
+            status: "INPROGRESS",
+            admin:0,
+            employee:0,
+            category : form.value.categorie
+          }
+          // console.log(this.ndf)
+          this.httpService.createNdF(this.ndf).subscribe(x=> {
+            this.data = x
+            this.id = this.data.id
+            localStorage.setItem("idNdf",this.id)
+
+          })
+
+          this.idNdf = JSON.parse(localStorage.getItem("idNdf"))
+          for(let i=0;i< this.myFiles.length;i++ ){
+            console.log(this.myFiles[i])
+            this.formData = new FormData()
+            this.formData.append("file",this.myFiles[i])
+            this.httpService.createUpload(this.formData,this.idNdf).subscribe({
+              next : (y) =>{
+                console.log(y)
+              },
+              error:(err) =>  console.log(err.message)
+            })
+            this.formData = 0
+          }
+      }
+      this.myFiles = []
+    // this.route.navigate(['/mesndf'])
 
   }
+
+
+
+
+
 }
